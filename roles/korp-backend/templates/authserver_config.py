32e1802ba71c0b49783cd36cbea022ad2dccfc5e
Sam Hardwick <sam.hardwick@csc.fi>
2025-01-20 17:20:05 +0200

    KP-8688 authserver logging config w/syslog

--- roles/korp-backend/templates/authserver_config.py
+++ roles/korp-backend/templates/authserver_config.py
@@ -5,13 +5,16 @@
 # For log levels
 import logging
 
+LOG_USING_NATIVE_PYTHON = False
+LOG_USING_SYSLOG = True
+
 # Host and port for the WSGI server
 WSGI_HOST = "0.0.0.0"
 WSGI_PORT = 1235
 
 # Database host and port
 DBHOST = "{{ korp_db_server }}"
-DBPORT = {{ korp_db_port }}
+DBPORT = {{korp_db_port}}
 # Database name
 DBNAME = "korp_auth"
 # Username and password for database access
